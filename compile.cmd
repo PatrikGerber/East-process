f1f9af58d9e6afc95f42edd84d49b32b10aeb383
PatrikGerber <patrikgerber96@gmail.com>
2018-10-15 15:51:36 +0100

    Final version of writeup and the presentation

--- compile.cmd
+++ compile.cmd
@@ -2,7 +2,7 @@ title Compiling Latex into PDF
 :: Takes one command line argument, the name of the tex file you want to compile
 set filename=%1
 pdflatex %filename%
-bibtex %filename%
-pdflatex %filename%
-pdflatex %filename%
+rem bibtex %filename%
+rem pdflatex %filename%
+rem pdflatex %filename%
 start %filename%.pdf
